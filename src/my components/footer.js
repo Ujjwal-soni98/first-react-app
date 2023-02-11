166ec94cf1c0f9e769a30cd4b67ce53152e721a2
Ujjwal-soni-98 <ujjwal.soniji@gmail.com>
2023-02-11 12:24:04 +0530

    new commit

--- src/my components/footer.js
+++ src/my components/footer.js
@@ -1,11 +1,12 @@
 import React,{Component} from 'react'
 
-function footer() {
-  return (
-    <div>
-        <h2>I'm the footer component</h2>
-    </div>
-  )
-}
+export default class Footer extends Component{
+    render() {
+        return (
+            <div>
+                <h2>I'm the footer component</h2>
+            </div>
+        )
+    }
+} 
 
-export default footer
